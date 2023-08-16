86242d242394de9b31c8131c77f5f63cdf800c71
vishu-roy12 <45840201+vishu-roy12@users.noreply.github.com>
2023-08-15 23:26:13 -0400

    Update userapi.py

--- app/userapi.py
+++ app/userapi.py
@@ -12,7 +12,7 @@ app.config["MYSQL_DATABASE_USER"] = "root"
 app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
 app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
 app.config["MYSQL_DATABASE_HOST"] = os.getenv("host")
-app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("port"))
+app.config["MYSQL_DATABASE_PORT"] = os.getenv("port")
 mysql.init_app(app)
 
 
